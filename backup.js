168aa7b2799f9cd233966f3c1334902580088b10
Rahul Patwa <rahul.patwa@automationedge.com>
2023-03-10 09:29:58 +0530

    pushing with the build

---
+++ backup.js
@@ -0,0 +1,71 @@
+const localtunnel = require('localtunnel');
+const chalk = require('chalk');
+const promptSync = require('prompt-sync')();
+const fs = require('fs');
+
+async function connect(){
+  
+  var port;
+  var subdomain;
+  const path = "./lt_config";
+  var c = {}
+
+  
+  const funccall = async(port,subdomain) => {  
+    console.log('port,subdomain')
+    console.log(port,subdomain)
+    try {
+      const tunnel = await localtunnel({ port, subdomain });
+      console.log(`Tunnel created at: ${tunnel.url}`);
+    } catch (error) {
+      console.error('Error creating tunnel:', error);
+    }
+  }
+
+  if (fs.existsSync(path)) {
+    fs.readFile('./lt_config/config.json', 'utf8', (err, data) => {
+      let jsonData = JSON.parse(data);
+      port = jsonData.port
+      subdomain  = jsonData.addr
+      c = {port, subdomain}
+      funccall(port, subdomain)
+    })
+  }
+  else{
+    const regex = /^[0-9]{4,}$/;
+    do {
+      port = promptSync(chalk.yellow('Configure port that you want to expose '));
+      } 
+      while (!regex.test(port));
+      
+    subdomain = promptSync(chalk.yellow('Configure a customized DNS '))
+    
+    if(!subdomain ){
+      console.log(chalk.white('Since not customized DNS was given, a random string would be generated'))
+    }
+    
+    if(subdomain && /\./.test(subdomain )){
+      subdomain  = promptSync(chalk.red("Customized DNS can't have (.) in between "))
+    }
+    
+    c = {port, subdomain}
+    funccall(port, subdomain)
+
+    await fs.promises.mkdir('lt_config', { recursive: true })
+    let Configuration = {
+          "port": port,
+          "addr" : subdomain,
+          "proto" : "http"
+      }
+    await fs.promises.writeFile('lt_config/config.json',JSON.stringify(Configuration))
+  }
+
+
+  // try {
+  //   const tunnel = await localtunnel({ port, subdomain });
+  //   console.log(`Tunnel created at: ${tunnel.url}`);
+  // } catch (error) {
+  //   console.error('Error creating tunnel:', error);
+  // }
+}
+connect()
--- index.js
+++ index.js
@@ -1,67 +1,64 @@
-#!/usr/bin/env node
-
 const localtunnel = require('localtunnel');
 const chalk = require('chalk');
 const promptSync = require('prompt-sync')();
 const fs = require('fs');
-const os = require('os');
 
 async function connect(){
+  
+  var port;
+  var subdomain;
   const path = "./lt_config";
-    var port 
-    var subdomain 
-    if (fs.existsSync(path)) {
-        fs.readFile('./lt_config/config.json', 'utf8', (err, data) => {
-          if (err) {
-            console.error(err);
-            process.exit(1);
-          }
-            let jsonData = JSON.parse(data);
-            port = jsonData.port
-            subdomain  = jsonData.addr
-            const options  = subdomain  ? { subdomain  } : {};
-            localtunnel(port,options, (err, tunnel) => {
-              if (err) {
-                console.error(err);
-                process.exit(1);
-              }
-              console.log(chalk.underline('Give me moment while I create a tunnel'))
-              console.log(chalk.bgWhite(chalk.black(`Forwarding ${tunnel.url}  -> http://localhost:${port}`)));
-            })
-          });
-        } else {
-        const regex = /^[0-9]{4,}$/;
-        do {
-          port = promptSync(chalk.yellow('Configure port that you want to expose '));
-        } while (!regex.test(port));
+  var c = {}
 
-        subdomain = promptSync(chalk.yellow('Configure a customized DNS '))
-
-        if(!subdomain ){
-          console.log(chalk.white('Since not customized DNS was given, a random string would be generated'))
-        }
-
-        if(subdomain && /\./.test(subdomain )){
-          subdomain  = promptSync(chalk.red("Customized DNS can't have (.) in between "))
-        }
-        
-        const options  = subdomain  ? { subdomain  } : {};
-        localtunnel(port,options, (err, tunnel) => {
-          console.log(chalk.underline('Give me moment while I create a tunnel'))
-          if (err) {
-            console.error(err);
-            process.exit(1);
-          }
-            console.log(chalk.bgWhite(chalk.black(`Forwarding ${tunnel.url} -> http://localhost:${port}`)));
-        })
-        await fs.promises.mkdir('lt_config', { recursive: true })
-        let Configuration = {
-            "port": port,
-            "addr" : subdomain,
-            "proto" : "http"
-        }
-        await fs.promises.writeFile('lt_config/config.json',JSON.stringify(Configuration))
+  
+  const funccall = async(port,subdomain) => {  
+    console.log('port,subdomain')
+    console.log(port,subdomain)
+    try {
+      const tunnel = await localtunnel({ port, subdomain });
+      console.log(`Tunnel created at: ${tunnel.url}`);
+    } catch (error) {
+      console.error('Error creating tunnel:', error);
     }
+  }
+
+  if (fs.existsSync(path)) {
+    fs.readFile('./lt_config/config.json', 'utf8', (err, data) => {
+      let jsonData = JSON.parse(data);
+      port = jsonData.port
+      subdomain  = jsonData.addr
+      c = {port, subdomain}
+      funccall(port, subdomain)
+    })
+  }
+  else{
+    const regex = /^[0-9]{4,}$/;
+    do {
+      port = promptSync(chalk.yellow('Configure port that you want to expose '));
+      } 
+      while (!regex.test(port));
+      
+    subdomain = promptSync(chalk.yellow('Configure a customized DNS '))
+    
+    if(!subdomain ){
+      console.log(chalk.white('Since not customized DNS was given, a random string would be generated'))
+    }
+    
+    if(subdomain && /\./.test(subdomain )){
+      subdomain  = promptSync(chalk.red("Customized DNS can't have (.) in between "))
+    }
+    
+    c = {port, subdomain}
+    funccall(port, subdomain)
+
+    await fs.promises.mkdir('lt_config', { recursive: true })
+    let Configuration = {
+          "port": port,
+          "addr" : subdomain,
+          "proto" : "http"
+      }
+    await fs.promises.writeFile('lt_config/config.json',JSON.stringify(Configuration))
+  }
 
 }
 connect()
